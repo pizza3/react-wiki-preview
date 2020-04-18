68e537a40f991a35d57f2bdc20efa720565be3bc
pizza3 <yugam.dhuriya@gmail.com>
2020-04-18 12:03:52 +0530

    UPDATE: added a new file structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import Wrapper from './components/Wrapper'
 
-interface Props {
-  text: string
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export default Wrapper
